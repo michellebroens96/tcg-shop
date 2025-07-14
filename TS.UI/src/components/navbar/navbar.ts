d445ff3e406cabb5cdbd796735a7611a0efcd68e
Michelle <michellebroens96@gmail.com>
2025-07-14 15:35:00 +0200

    navbar base

--- TS.UI/src/components/navbar/navbar.ts
+++ TS.UI/src/components/navbar/navbar.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Menubar } from 'primeng/menubar';
+import { Badge } from 'primeng/badge';
+import { NgClass } from '@angular/common';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [Menubar, Badge, NgClass],
   templateUrl: './navbar.html',
 })
-export class Navbar {}
+export class Navbar {
+  items: MenuItem[] = [
+    {
+      label: 'Home',
+      icon: 'pi pi-home',
+    },
+    {
+      label: 'Settings',
+      icon: 'pi pi-cog',
+    },
+  ];
+}
