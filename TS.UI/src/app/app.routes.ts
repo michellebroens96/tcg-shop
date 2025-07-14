28a0316efbb524d9764a4b04d3e3f174cfd6d520
Michelle <michellebroens96@gmail.com>
2025-07-14 14:30:55 +0200

    added navbar + home page + cart page

--- TS.UI/src/app/app.routes.ts
+++ TS.UI/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { CartComponent } from '../pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    title: 'Cart',
+  }
+];
