92fbe52bfd0ebe6878f7d45682031b17a91c52dd
Michelle <michellebroens96@gmail.com>
2025-07-14 14:43:03 +0200

    some fixes

--- TS.UI/src/pages/home/home.component.ts
+++ TS.UI/src/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
